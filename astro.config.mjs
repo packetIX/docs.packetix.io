44cb8e2e701b9e05be8255abf95ed8361d51b1f8
Alex4386 <psh010209@gmail.com>
2024-09-09 16:13:23 +0900

    ci: make it cloudflare pages ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,50 +2,56 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://docs.packetix.io',
-	integrations: [
-		starlight({
-			title: 'packet:IX Docs',
-			customCss: [
-				'./src/styles/custom.css',
-				'./src/styles/fonts.css',
-			],
-			locales: {
-				en: {
-					label: 'English',
-				},
-				ko: {
-					label: '한국어',
-				},
-				ja: {
-					label: '日本語',
-				},
-			},
-			defaultLocale: 'en',
-			social: {
-				github: 'https://github.com/packetIX/docs.packetix.io',
-				discord: 'https://packetix.io/discord',
-			},
-			editLink: {
-				baseUrl: 'github.com/packetIX/docs.packetix.io/edit/main/docs/',
-			},
-			logo: {
-				light: './src/assets/logo-light.svg',
-				dark: './src/assets/logo-dark.svg',
-				replacesTitle: true,
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides' },
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+  site: 'https://docs.packetix.io',
+
+  integrations: [
+      starlight({
+          title: 'packet:IX Docs',
+          customCss: [
+              './src/styles/custom.css',
+              './src/styles/fonts.css',
+          ],
+          locales: {
+              en: {
+                  label: 'English',
+              },
+              ko: {
+                  label: '한국어',
+              },
+              ja: {
+                  label: '日本語',
+              },
+          },
+          defaultLocale: 'en',
+          social: {
+              github: 'https://github.com/packetIX/docs.packetix.io',
+              discord: 'https://packetix.io/discord',
+          },
+          editLink: {
+              baseUrl: 'github.com/packetIX/docs.packetix.io/edit/main/docs/',
+          },
+          logo: {
+              light: './src/assets/logo-light.svg',
+              dark: './src/assets/logo-dark.svg',
+              replacesTitle: true,
+          },
+          sidebar: [
+              {
+                  label: 'Guides',
+                  autogenerate: { directory: 'guides' },
+              },
+              {
+                  label: 'Reference',
+                  autogenerate: { directory: 'reference' },
+              },
+          ],
+      }),
 	],
+
+  output: 'server',
+  adapter: cloudflare(),
 });
